00e139e921cac2ab66d63bbe02ed955257d5badc
mariantulivskyi <mariantulivskyi@gmail.com>
2022-05-13 19:10:58 +0300

    Homework-1 Gitbash

---
+++ homework.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#sctript HW-Gitbash
+mkdir gitbash-script
+cd gitbash-script
+mkdir gb-scrpt-here
+cd gb-scrpt-here
+touch info{1..3}.txt footer.txt title.txt
+mkdir background{1..3}
+ls -la
+mv info1.txt title.txt ~/test-script/gbash
